b85b38a2b2ba6d427a0eadd2e47e2261c36d5f24
shenziy <863454567@qq.com>
2022-10-14 12:01:00 +0800

    modified proxy

--- backend/server/config/allowedOrigins.js
+++ backend/server/config/allowedOrigins.js
@@ -4,7 +4,7 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
     "http://localhost:8000",
-    '*'
+    "http://42.244.24.75:8090"
 ];
 
 module.exports = allowedOrigins;
