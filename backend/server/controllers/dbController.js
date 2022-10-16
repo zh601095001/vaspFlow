1960b16d9a0bd15365313801ced24986f3665004
zhaohao <601095001@qq.com>
2022-10-16 18:11:44 +0800

    modified mongodb volumes map

--- backend/server/controllers/dbController.js
+++ backend/server/controllers/dbController.js
@@ -1,67 +1,60 @@
 const status = require('http-status')
 
 const getItems = async (req, res) => {
-	const {limit, skip, ...extraQuery} = req.query
-	const filter = {}
-	Object.entries(extraQuery).forEach(q => {
-		if (q[0].startsWith("min_")) {
-			const key = q[0].slice(4)
-			filter[key] = {$lt: Number(q[1])}
-		} else if (q[0].startsWith("max_")) {
-			const key = q[0].slice(4)
-			filter[key] = {$gte: Number(q[1])}
-		} else {
-			const v = q[1]
-			filter.$or = [
-				{
-					[q[0]]: {$regex: new RegExp(`${v}`)}
-				},
-				{
-					[q[0]]: parseInt(v)
-				}
-			]
-		}
-	})
-	const cur = req.collection.find(filter).skip(skip ? Number(skip) : 0).limit(limit ? Number(limit) : 10)
-	const data = await cur.toArray()
-	res.json(data)
+    let {limit, skip, ...extraQuery} = req.query
+    limit = Number(limit)
+    skip = Number(skip)
+    const cur = req.collection.find(extraQuery).skip(skip ? skip : 0).limit(limit ? limit : 10)
+    const data = await cur.toArray()
+    const count = await req.collection.countDocuments()
+    res.json({data, count, skip, limit})
 }
 const addItems = async (req, res) => {
-	const data = req.body
-	if (data instanceof Array) {
-		try {
-			res.json(await req.collection.insertMany(req.body))
-		} catch (e) {
-			res.status(status.CONFLICT).json({
-				msg: e.toString()
-			})
-		}
-	}
+    const data = req.body
+    if (data instanceof Array) {
+        try {
+            res.json(await req.collection.insertMany(req.body))
+        } catch (e) {
+            res.status(status.CONFLICT).json({
+                msg: e.toString()
+            })
+        }
+    } else {
+        res.status(status.UNPROCESSABLE_ENTITY).json({
+            errMsg: "参数格式不正确"
+        })
+    }
 }
 const modifyItems = async (req, res) => {
-	const {idName} = req.query
-	const info = req.body.map(async item => {
-		const identify = idName ? idName : "id"
-		return await req.collection.updateMany({[identify]: item[identify]}, {
-			$set: item
-		})
-	})
-	res.json(await Promise.all(info))
+    const {idName} = req.query
+    if (!(req.body instanceof Array || req.body instanceof Object)) {
+        return res.status(status.UNPROCESSABLE_ENTITY).json({
+            errMsg: "参数格式不正确"
+        })
+    }
+    const updateOne = async ({item, idName}) => {
+        const identify = idName ? idName : "id"
+        return await req.collection.updateMany({[identify]: item[identify]}, {
+            $set: item
+        })
+    }
+    const info = req.body.map(item => updateOne({item, idName}))
+    return res.json(await Promise.all(info))
 }
 
 const deleteItems = async (req, res) => {
-	const {idName} = req.query
-	const identify = idName ? idName : "id"
-	const info = req.body.map(async item => {
-		return await req.collection.deleteMany({[identify]: item[identify]})
-	})
-	res.json(await Promise.all(info))
+    const {idName} = req.query
+    const identify = idName ? idName : "id"
+    const info = req.body.map(async item => {
+        return await req.collection.deleteMany({[identify]: item[identify]})
+    })
+    res.json(await Promise.all(info))
 }
 module.exports = {
-	getItems,
-	modifyItems,
-	deleteItems,
-	addItems
+    getItems,
+    modifyItems,
+    deleteItems,
+    addItems
 }
 
 
--- backend/server/test.js
+++ backend/server/test.js
@@ -1,4 +1,14 @@
-const obj = {a: 1, b: 2, c: 3}
-const {a,...extras} = obj
-
-console.log(extras)
+class A{
+    // [Symbol.toStringTag] = "A"
+    constructor(arg1) {
+        this.arg1 = arg1
+        this[Symbol.toStringTag] = "A"
+    }
+    log(){
+        console.log(this.arg1)
+    }
+}
+let a = new A()
+a.log()
+console.log(Object.prototype.toString.call(a));
+console.log(a.constructor)
